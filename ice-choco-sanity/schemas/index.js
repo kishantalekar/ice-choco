00a0bb3bbe9a696ec887801d4e8773b1abc708e1
kishantalekar <kishantalekar024@gmail.com>
2023-08-13 00:53:19 +0530

    removed sanity folder

--- ice-choco-sanity/schemas/index.js
+++
@@ -1,7 +0,0 @@
-import brand from './brand'
-import category from './category'
-import iceCream from './iceCream'
-import Featured from './Featured'
-import Carousel from './Carousel'
-import users from './users'
-export const schemaTypes = [brand, category, iceCream, Featured, Carousel]
